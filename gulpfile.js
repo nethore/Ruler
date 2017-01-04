fa39d8cea193f4808c798f4147b2d4059142a5b7
nethore <nikolas.ethore@gmail.com>
2017-01-04 10:30:05 +0100

    Push Original

---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+"use strict";
+
+
+/**
+ * This example:
+ *  Uses the built-in BrowserSync server for HTML files
+ *  Watches & compiles SASS files
+ *  Watches & injects CSS files
+ */
+var browserSync = require('browser-sync');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var size = require('gulp-size');
+var uncss = require('gulp-uncss');
+var filter = require('gulp-filter');
+var jsmin = require('gulp-jsmin');
+var concat = require('gulp-concat');
+var notify = require("gulp-notify");
+var minifyCss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+var cache = require('gulp-cache');
+var imagemin = require('gulp-imagemin');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+
+
+//init tyo reload brower
+var reload = browserSync.reload;
+
+
+// Browser-sync task, only cares about compiled CSS
+gulp.task('browser-sync', function() {
+    browserSync({
+        port: 8066,
+        browser: "Chromium",
+        server: {
+            baseDir: "./", //base
+            index: "index.html" //fichier a chargé
+        }
+    });
+});
+
+// reload a server
+gulp.task('browser-reload', function() {
+    reload({ stream: true, reload: once });
+});
+
+// Clean log, comments, remove old files
+gulp.task('clean', function() {
+    return gulp.src(['dist/css', 'dist/js'], { read: false })
+        .pipe(clean());
+});
+
+// Sass task, will run when any SCSS files change.
+gulp.task('css', function() {
+    return gulp.src('sass/main.scss')
+        .pipe(plumber({
+            errorHandler: notify.onError("Error: <%= error.message %>")
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(sass({ style: 'expanded', }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifyCss())
+        // .pipe(uncss({
+        //         html: ['*.html']
+        //     }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/css/'));
+        // .pipe(notify("Style Modifié"));
+});
+
+//For js
+gulp.task('js', function() {
+    // Order By initi, filters, controllers...
+    return gulp.src([ //ordre a respecter
+            "js/*.js",
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(plumber({
+            errorHandler: notify.onError("Error: <%= error.message %>")
+        })) // débogage de mes pipes
+        .pipe(concat('app.min.js'))
+        .pipe(sourcemaps.write('.'))
+        // .pipe(jsmin())
+        .pipe(gulp.dest('dist/js')); // repertoire distant
+        // .pipe(notify("Js Modifié")); // notification
+});
+
+
+// Default task to be run with `gulp`
+gulp.task('default', ['browser-sync', 'css', 'js'], function() {
+    gulp.watch("sass/**/*.scss", ['css']); // watch permet de regarder les changements de fichier et lancer les tâches que l'on souhaite
+    // gulp.watch("assets/images/", ['images']);
+    gulp.watch("js/**/**/*.js", ['js']);
+    // gulp.watch(["*.html", "partials/*.html"]).on('change', browserSync.reload); //reload on HTML
+    gulp.src('*').pipe(size());
+
+});
---
+++ js/main.js
@@ -0,0 +1,164 @@
+$(document).ready(function()
+{
+
+	// Gestion des tailles
+
+	var taille1 = 185;
+	var taille2 = 165;
+
+	$('.p:nth-child(1)').find('.taille').text(taille1);
+	$('.p:nth-child(2)').find('.taille').text(taille2);
+
+	$('.p:nth-child(1)').css('height', function() {
+		return ((taille1 - 5) * 100) / 225 + "%";
+	});
+
+	$('.p:nth-child(2)').css('height', function() {
+		return ((taille2 - 5) * 100) / 225 + "%";
+	});
+
+	$('.p:nth-child(1) img').css('height', function() {
+		return (taille1 * 25) / 225 + "%";
+	});
+
+	$('.p:nth-child(2) img').css('height', function() {
+		return (taille2 * 25) / 225 + "%";
+	});
+
+
+
+	$(".close").css("display", "none");
+	$(".close-help").css("display", "none");
+
+	var isMenuOpen = false;
+	var isHelpOpen = false;
+	var isAddOpen = false;
+	var isAddRwdOpen = false;
+
+
+
+	$('.menu_btn').click(function()
+	{
+		if (isMenuOpen === false)
+		{
+		//alert('je suis dans le bon cas')
+			$("#menu").clearQueue().animate({
+				left : '0'
+			});
+
+			$(".close").fadeIn(300);
+
+			isMenuOpen = true;
+		}
+	});
+
+	$('.close').click(function()
+	{
+		if (isMenuOpen === true)
+		{
+			$("#menu").clearQueue().animate({
+				left : '-240px'
+			});
+
+			$(this).fadeOut(200);
+
+			isMenuOpen = false;
+		}
+	});
+
+  $('.close-add').click(function()
+	{
+		if (isAddOpen === false)
+		{
+			$(this).clearQueue().animate({
+				width : '55px',
+				left: '-55px'
+			});
+			$("#admin").clearQueue().animate({
+				right : '0'
+			});
+			$('#admin').find('.icn-admin').removeClass('icon-add-man').addClass('icon-close-bold');
+			$('#admin').find('.txt-admin').hide();
+
+			isAddOpen = true;
+		}
+		else if (isAddOpen === true)
+		{
+			$(this).clearQueue().animate({
+				width : '160px',
+				left: '-160px'
+
+			});
+			$("#admin").clearQueue().animate({
+				right : '-280px'
+			});
+			$('#admin').find('.icn-admin').addClass('icon-add-man').removeClass('icon-close-bold');
+			$('#admin').find('.txt-admin').show();
+
+
+			isAddOpen = false;
+		}
+	});
+
+	$('.close-rwd').click(function()
+	{
+		if (isAddRwdOpen === false)
+		{
+			$(this).clearQueue().animate({
+				width : '55px'
+			});
+			$("#admin-rwd").clearQueue().animate({
+				top : '0'
+			});
+			$('#admin-rwd').find('.icn-admin').removeClass('icon-add-man').addClass('icon-close-bold');
+			$('#admin-rwd').find('.txt-admin').hide();
+
+			isAddRwdOpen = true;
+		}
+		else if (isAddRwdOpen === true)
+		{
+			$(this).clearQueue().animate({
+				width : '160px'
+
+			});
+			$("#admin-rwd").clearQueue().animate({
+				top : '-500px'
+			});
+			$('#admin-rwd').find('.icn-admin').addClass('icon-add-man').removeClass('icon-close-bold');
+			$('#admin-rwd').find('.txt-admin').show();
+
+
+			isAddRwdOpen = false;
+		}
+	});
+
+	$('.help_btn').click(function()
+	{
+		if (isHelpOpen === false)
+		{
+		//alert('je suis dans le bon cas')
+			$("#help").clearQueue().animate({
+				right : '0'
+			});
+
+			$(".close-help").fadeIn(300);
+
+			isHelpOpen = true;
+		}
+	});
+
+	$('.close-help').click(function()
+	{
+		if (isHelpOpen === true)
+		{
+			$("#help").clearQueue().animate({
+				right : '-240px'
+			});
+
+			$(this).fadeOut(200);
+
+			isHelpOpen = false;
+		}
+	});
+
+});
